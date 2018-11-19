18d1435c6e6b6c3738ac2397c7b52e5bfec4fef2
lfernandez@funiglobal.com <lfernandez@funiglobal.com>
2018-11-19 14:50:02 +0100

    First commit

---
+++ src/formats.ts
@@ -0,0 +1,80 @@
+import { format } from 'winston';
+import { TransformableInfo } from 'logform';
+import * as util from 'util';
+
+const colors = {
+    RED: '\u001b[31m',
+    GREEN: '\u001b[32m',
+    YELLOW: '\u001b[33m',
+    BLUE: '\u001b[34m',
+    MAGENTA: '\u001b[35m',
+    CYAN: '\u001b[36m',
+    WHITE: '\u001b[37m',
+    DEFAULT: '\u001b[39m'
+};
+
+const PREFIX_CHAR: string = '\u25CF';
+
+const colorPallete = [
+    colors.YELLOW,
+    colors.MAGENTA,
+    colors.CYAN,
+    colors.RED,
+    colors.GREEN,
+    colors.BLUE,
+    colors.WHITE
+];
+
+let currentColor = 0;
+
+
+function formatType(type: any) {
+    switch (typeof (type)) {
+        case 'undefined': return 'undefined';
+        case 'object': return util.inspect(type);
+    }
+    return type;
+}
+
+function formatMeta(meta: any) {
+    if (Array.isArray(meta)) {
+        if (meta.length == 0) return '[]';
+        return meta.map((cur, idx) => `\n${idx}: ${formatType(cur)}`);
+    }
+    return formatType(meta);
+}
+
+const mergeArguments = format((info: TransformableInfo) => {
+    if (typeof(info.meta) != 'undefined') info.message = `${info.message}${formatMeta(info.meta)}`;
+    return info;
+});
+
+function formatLabel(label: string, color?: string) {
+    if (label) return `${label}| `;
+    return '';
+}
+
+function printPrefix (color: string) {
+    return `${color}${PREFIX_CHAR}${colors.DEFAULT}`;
+}
+
+/** Winston formats */
+function defaultFormat(opts: any = {}) {
+    const color: string = colorPallete[(currentColor++ % colorPallete.length)];
+    return format.combine(
+        format.label({label: opts.label}),
+        format.colorize(),
+        format.splat(),
+        format.timestamp({ format: 'DD/MM/YYYY HH:mm:ss.SSS' }),
+        mergeArguments(),
+        format.printf(info => `${info.timestamp} ${printPrefix(color)} ${formatLabel(info.label, color)}${info.level}: ${info.message}`)
+    );
+}
+export const winstonFormats = {
+    /** defaultFormat
+     * Merge all arguments into one single line.
+     * Format description: timestamp - level - message
+     */
+    defaultFormat: defaultFormat
+};
+
---
+++ src/index.ts
@@ -0,0 +1,1 @@
+export { logger, createNamespace } from './winston';
